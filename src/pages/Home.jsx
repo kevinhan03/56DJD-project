1ac7abed0ef0fe11dee48e217031db32e3749e40
kevinhan03 <93977275+kevinhan03@users.noreply.github.com>
2024-08-31 14:10:22 +0000

    Pending changes exported from your codespace

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Home.module.css';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to the Home page!</p>
+        </div>
+    );
+}
